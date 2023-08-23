3f548f8d6aa44cec8a0ac80c41721481b23bace1
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2023-08-23 13:33:32 +0800

    Update errors.go

--- internal/api/errors.go
+++ internal/api/errors.go
@@ -7,6 +7,16 @@ import (
 
 var ErrDuplicateAPNSToken = errors.New("duplicate apns token")
 
+type ErrorResponse struct {
+	Message string      `json:"message"`
+	Errors  []FieldError  `json:"errors"`
+}
+
+type FieldError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
 func (a *api) errorResponse(w http.ResponseWriter, _ *http.Request, status int, err error) {
 	w.Header().Set("X-Budgetto-Error", err.Error())
 	http.Error(w, err.Error(), status)
