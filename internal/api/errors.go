46a356094de1b30a4e5e4ee4a42a18b4dd3cb1f7
Brixter Porras <brixterporras@gmail.com>
2023-08-24 12:21:52 +0800

    feat: updated error handler

--- internal/api/errors.go
+++ internal/api/errors.go
@@ -1,15 +1,18 @@
 package api
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/go-playground/validator"
 )
 
-var ErrDuplicateAPNSToken = errors.New("duplicate apns token")
-
 type ErrorResponse struct {
-	Message string      `json:"message"`
-	Errors  []FieldError  `json:"errors"`
+	Message string       `json:"message"`
+	Errors  []FieldError `json:"errors,omitempty"`
 }
 
 type FieldError struct {
@@ -18,6 +21,60 @@ type FieldError struct {
 }
 
 func (a *api) errorResponse(w http.ResponseWriter, _ *http.Request, status int, err error) {
-	w.Header().Set("X-Budgetto-Error", err.Error())
-	http.Error(w, err.Error(), status)
+	var errorResponse ErrorResponse
+
+	switch typedErr := err.(type) {
+	case error:
+		var message string
+		if status == 500{
+			log.Println(typedErr)
+			message =  "Something went wrong!"
+		}else{
+			message = typedErr.Error()
+		}
+
+		errorResponse = ErrorResponse{
+			Message: message,
+			Errors:  []FieldError{},
+		}
+	case validator.ValidationErrors:
+		errorFields := []FieldError{}
+		for _, e := range typedErr {
+			errorFields = append(errorFields, FieldError{
+				Field:   e.Field(),
+				Message: GetValidationErrorMessage(e),
+			})
+		}
+
+		errorResponse = ErrorResponse{
+			Message: "Validation Error",
+			Errors:  errorFields,
+		}
+	}
+
+	errorResJson, _ := json.Marshal(errorResponse)
+
+	// w.Header().Set("X-Budgetto-Error", err.Error())
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(errorResJson)
+}
+
+func GetValidationErrorMessage(err validator.FieldError) string {
+	fieldName := err.Field()
+	switch err.Tag() {
+	case "required":
+		return fmt.Sprintf("%s field is required.", fieldName)
+	case "min":
+		minValue, _ := strconv.Atoi(err.Param())
+		return fmt.Sprintf("%s should be at least %d characters long.", fieldName, minValue)
+	case "max":
+		maxValue, _ := strconv.Atoi(err.Param())
+		return fmt.Sprintf("%s should be at most %d characters long.", fieldName, maxValue)
+	case "email":
+		return "Enter a valid email address."
+	// Add more cases for other validation tags as needed.
+	default:
+		return "Invalid input."
+	}
 }
