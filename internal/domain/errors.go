fc66a42eaee3d57dd19f86a19e7e70ca3ba9284a
Brixter Porras <brixterporras@gmail.com>
2023-08-26 18:48:55 +0800

    style: format errors var

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,12 +3,8 @@ package domain
 import "errors"
 
 var (
-	// ErrNotFound will be returned if the requested item is not found
-	ErrNotFound  = errors.New("Requested item was not found.")
-	ErrForbidden = errors.New("You don't have permission to access the requested resource.")
-	// ErrConflict will be returned if the item being persisted already exists
-	ErrConflict = errors.New("Item already exists.")
-
+	ErrNotFound           = errors.New("Requested item was not found.")
+	ErrForbidden          = errors.New("You don't have permission to access the requested resource.")
 	ErrInvalidCredentials = errors.New("Invalid credentials. Please try again.")
 )
 
