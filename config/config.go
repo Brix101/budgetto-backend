c0fd998e0a7498293a9f0714c0f7ca7d9fe77b73
Brixter Porras <brixterporras@gmail.com>
2023-08-31 18:26:28 +0800

    feat: added env checker on config

--- config/config.go
+++ config/config.go
@@ -23,9 +23,13 @@ func GetConfig() *EnvVars {
 }
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	env := os.Getenv("GO_ENV")
+
+	if env == "prod" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	envType := reflect.TypeOf(&config).Elem()
