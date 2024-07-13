b6dc770c094016d8b8e638bee7229e1304ab4552
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2024-07-13 14:00:21 +0800

    Update api.go

--- internal/cmd/api.go
+++ internal/cmd/api.go
@@ -11,35 +11,7 @@ import (
 
 	"github.com/Brix101/budgetto-backend/internal/api"
 	"github.com/Brix101/budgetto-backend/internal/util"	
-    "go.opentelemetry.io/otel"
-    "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-    "go.opentelemetry.io/otel/sdk/resource"
-    sdktrace "go.opentelemetry.io/otel/sdk/trace"
-    semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
-
-func initTracer() func() {
-    exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    tp := sdktrace.NewTracerProvider(
-        sdktrace.WithBatcher(exporter),
-        sdktrace.WithResource(resource.NewWithAttributes(
-            semconv.SchemaURL,
-            semconv.ServiceNameKey.String("example-service"),
-        )),
-    )
-
-    otel.SetTracerProvider(tp)
-
-    return func() {
-        if err := tp.Shutdown(context.Background()); err != nil {
-            log.Fatal(err)
-        }
-    }
-}
 
 func APICmd(ctx context.Context) *cobra.Command {
 	var port int
