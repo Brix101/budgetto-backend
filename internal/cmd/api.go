36c0397f3a9098d29a5737804393ece63a2db56b
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2024-07-13 11:36:25 +0800

    Update api.go

--- internal/cmd/api.go
+++ internal/cmd/api.go
@@ -4,16 +4,47 @@ import (
 	"context"
 	"os"
 	"strconv"
+	 "log"
 
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 
 	"github.com/Brix101/budgetto-backend/internal/api"
-	"github.com/Brix101/budgetto-backend/internal/util"
+	"github.com/Brix101/budgetto-backend/internal/util"	
+    "go.opentelemetry.io/otel"
+    "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+    "go.opentelemetry.io/otel/sdk/resource"
+    sdktrace "go.opentelemetry.io/otel/sdk/trace"
+    semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
+
+func initTracer() func() {
+    exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    tp := sdktrace.NewTracerProvider(
+        sdktrace.WithBatcher(exporter),
+        sdktrace.WithResource(resource.NewWithAttributes(
+            semconv.SchemaURL,
+            semconv.ServiceNameKey.String("example-service"),
+        )),
+    )
+
+    otel.SetTracerProvider(tp)
+
+    return func() {
+        if err := tp.Shutdown(context.Background()); err != nil {
+            log.Fatal(err)
+        }
+    }
+}
 
 func APICmd(ctx context.Context) *cobra.Command {
 	var port int
+	    cleanup := initTracer()
+    		defer cleanup()
 
 	cmd := &cobra.Command{
 		Use:   "api",
